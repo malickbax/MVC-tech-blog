0f0088eab1e18d05d3553d6a93a4d2f4e5d31f8d
malickbax <malickbax@gmail.com>
2022-10-07 00:31:23 -0400

    Added handlebars

---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+// Redirecting user to the login page
+const withAuth = (req, res, next) => {
+    if(!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+module.exports = withAuth;
