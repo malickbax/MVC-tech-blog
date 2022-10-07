79dfac3e3e57b2ff66f172b5b1bed7dd664161f0
malickbax <malickbax@gmail.com>
2022-10-07 09:22:31 -0400

    Attempted fix error message on Heroku URL by changing port number

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 // CREDIT: Worked on this part with the help of coder colleague Brams Lo
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
-    cookie: { maxAge: 36000 },
+    cookie: { maxAge: 86400 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
