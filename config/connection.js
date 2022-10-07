cfe5d4c2a610261dc3c2524078ba7cf27e930506
malickbax <malickbax@gmail.com>
2022-10-06 22:51:40 -0400

    Added API routes and user routes

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Declaring the sequelize const
+const Sequelize = require('sequelize');
+
+// Importing dotenv dependency
+require('dotenv').config();
+
+// Sequelize starts
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+module.exports = sequelize;
